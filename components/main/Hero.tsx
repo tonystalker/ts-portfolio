f4216cf73a27c9f3f0ea70f7b2ec04ed87324263
Ayush Tripathi <707ayushtripathi@gmail.com>
2024-04-11 18:19:11 +0530

    tsdropped

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col h-full w-full">
+    <div className="relative flex flex-col h-full w-full" id="about-me">
       <video
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px] left-0 z-[1] h-full object-cover"
+        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
       >
-        <source src="/blackhole.webm" type="video/wbm" />
+        <source src="/blackhole.wbm" type="video/webm" />
       </video>
     </div>
   );
