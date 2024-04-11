edfbd253baa51c13ba58c8bd6e6b9fe4d50441b2
Ayush Tripathi <707ayushtripathi@gmail.com>
2024-04-11 20:24:05 +0530

    ts-bombed

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import HeroContent from "../sub/HeroContent";
 
 const Hero = () => {
   return (
@@ -9,8 +10,9 @@ const Hero = () => {
         loop
         className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
       >
-        <source src="/blackhole.wbm" type="video/webm" />
+        <source src="/blackhole.webm" type="video/webm" />
       </video>
+      <HeroContent />
     </div>
   );
 };
